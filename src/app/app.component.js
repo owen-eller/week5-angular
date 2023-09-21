59e5ba5b0c6e06e9d1dfa7d2b87b667a42d4777c
Owen <oweneller@att.net>
2023-09-21 14:08:19 -0400

    angular

--- src/app/app.component.js
+++ src/app/app.component.js
@@ -1,50 +1,45 @@
-//toggle button
-const details = document.querySelector('summary');
+const duplicateButtons = document.querySelectorAll(".duplicateButton");
+const originalCard = document.querySelector(".container[data-card='1']");
+const toggleColorButton = document.querySelector(".toggleColorButton");
+const changeTitleButton = document.querySelector(".changeTitleButton");
+const deleteCardButton = document.querySelector(".deleteCardButton");
+const toggleDescriptionButtons = document.querySelectorAll(".toggle-button");
 
-document.querySelector('button[data-toggle-btn]').addEventListener('click', (e) => {
-  if (details.parentNode.getAttribute('open')) {
-        details.parentNode.removeAttribute('open');
-  }
-  else {
-    details.parentNode.setAttribute('open','open');    
+duplicateButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const cards = document.querySelectorAll(".container");
+    const clone = cards[0].cloneNode(true);
+    clone.querySelector(".toggle-button").classList.remove("hidden");
+    document.querySelector(".wrapper").appendChild(clone);
+
+    const toggleDescriptionButton = clone.querySelector(".toggle-button");
+    const cardDescription = clone.querySelector(".description");
+    toggleDescriptionButton.addEventListener("click", () => {
+      cardDescription.classList.toggle("hidden");
+    });
+  });
+});
+
+toggleColorButton.addEventListener("click", () => {
+  originalCard.classList.toggle("bg-toggle");
+});
+
+changeTitleButton.addEventListener("click", () => {
+  const cardTitle = originalCard.querySelector(".card-title");
+  cardTitle.textContent = "something else";
+});
+
+deleteCardButton.addEventListener("click", () => {
+  const cards = document.querySelectorAll(".container");
+  if (cards.length > 1) {
+    const lastCard = cards[cards.length - 1];
+    lastCard.remove();
   }
 });
 
-//duplicate button
-document.querySelector('.duplicate').addEventListener('click', function(e) {
-  const cardToClone = document.querySelector('.card').cloneNode(true);
-  document.body.appendChild(cardToClone);
+toggleDescriptionButtons.forEach((button, index) => {
+  button.addEventListener("click", () => {
+    const cardDescriptions = document.querySelectorAll(".description");
+    cardDescriptions[index].classList.toggle("hidden");
+  });
 });
-
-//delete button
-document.querySelector('#deletecard').addEventListener('click', function(e) {
-  let wantsTo = confirm("Are you sure?");
-  if (wantsTo) {
-    if (document.querySelector('.card:last-child') !== document.querySelector('.card')) {
-      document.querySelector('.card:last-child').remove();      
-    }
-    else {
-      alert("Cannot original last card");
-    }
-  }
-});
-
-//change title button
-document.querySelector('.changetxt').addEventListener('click', function(e) {
-  let name = "Something else";
-  if (name) {
-    document.querySelector('.card h3').innerText = name;
-  }
-});
-
-//change background
-const card = document.querySelector('#card');
-        let backgroundColor = false;
-    document.querySelector('#changebackground').addEventListener('click', function(e) {
-            if (backgroundColor) {
-                card.style.backgroundColor = 'white';
-            } else {
-                card.style.backgroundColor = 'gray';
-            }
-            backgroundColor = !backgroundColor;
-        });
